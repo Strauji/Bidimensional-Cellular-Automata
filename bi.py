4a23a5f44f7b1426405897ed2b1cd20d0ad97e66
Strauji <ezequiasrpg@gmail.com>
2015-03-23 20:45:16 -0300

    yet another simple update

--- bi.py
+++ bi.py
@@ -1,24 +1,26 @@
+#############
 import time
 import random
-############### This section define the global variables
+############ Variables/inits
 global mapc
-mapc = {}
 global x
-x = 20
 global y
 global turns
 global life
-turns = 0
+global updTime
+mapc = {}
+updTime = 0.0
+x = 0
+y = 0
 life = 0
-y =20
 turns = 0
-############### Here, we can see a way to handle a "2d array(huehue)"
-def initialize_CellMap(x,y):
+############################# Here, we can see a way to handle a "2d array(huehue)"
+def cellmap_initialize(x,y):
 	for i in range(0,x+1):
 		for j in range(0,y+1):
 			mapc[str(i)+','+str(j)] = '-'
-############## Return the map
-def get_Map():
+############### Return the map
+def map_get():
 	global x
 	global y
 	ret = ''
@@ -27,17 +29,22 @@ def get_Map():
 			ret = ret + mapc[str(j)+','+str(i)]
 		ret = ret + '\n'
 	return ret
-################ Initialize the 'Game'
-def init_life():
+################# Initialize the 'Game'
+def life_init():
 	global x
 	global y
+	global updTime
+	times = 0
 	x = int(raw_input('Enter X plane size:'))
 	y = int(raw_input('Enter Y plane size:'))
-	times = int(raw_input('Enter how much shapes you want to create:'))
-	initialize_CellMap(x,y)
+	while (updTime < 0.1):
+		updTime = float(raw_input('Enter the update time(0.1 min):'))
+	while (times <= 0):
+		times = int(raw_input('Enter how much shapes you want to create:'))
+	cellmap_initialize(x,y)
 	for i in range(0,times):
-		cX = int(raw_input('Enter a start(' + str(i) + ') position X:'))
-		cY = int(raw_input('Enter a start(' + str(i) + ') position Y:'))
+		cX = int(raw_input('Enter a start(' + str(i+1) + ') position X:'))
+		cY = int(raw_input('Enter a start(' + str(i+1) + ') position Y:'))
 	
 	
 		done = False
@@ -45,305 +52,266 @@ def init_life():
 			cType = raw_input('Enter a cell type[Star,HLine,VLine,T,V,+]:')
 			try:
 				if cType == 'Star':
-					cell_Create(str(cX+1)+','+str(cY+1))
-					cell_Create(str(cX+1)+','+str(cY-1))
-					cell_Create(str(cX-1)+','+str(cY+1))
-					cell_Create(str(cX-1)+','+str(cY-1))
-					cell_Create(str(cX)+','+str(cY))
+					cell_create(str(cX+1)+','+str(cY+1))
+					cell_create(str(cX+1)+','+str(cY-1))
+					cell_create(str(cX-1)+','+str(cY+1))
+					cell_create(str(cX-1)+','+str(cY-1))
+					cell_create(str(cX)+','+str(cY))
 					done = True
 				if cType == 'HLine':
-					cell_Create(str(cX+1)+','+str(cY))
-					cell_Create(str(cX+2)+','+str(cY))
-					cell_Create(str(cX-1)+','+str(cY))
-					cell_Create(str(cX-2)+','+str(cY))
-					cell_Create(str(cX)+','+str(cY))
+					cell_create(str(cX+1)+','+str(cY))
+					cell_create(str(cX+2)+','+str(cY))
+					cell_create(str(cX-1)+','+str(cY))
+					cell_create(str(cX-2)+','+str(cY))
+					cell_create(str(cX)+','+str(cY))
 					done = True
 				if cType == 'VLine':
-					cell_Create(str(cX)+','+str(cY+1))
-					cell_Create(str(cX)+','+str(cY-1))
-					cell_Create(str(cX)+','+str(cY+2))
-					cell_Create(str(cX)+','+str(cY-2))
-					cell_Create(str(cX)+','+str(cY))
+					cell_create(str(cX)+','+str(cY+1))
+					cell_create(str(cX)+','+str(cY-1))
+					cell_create(str(cX)+','+str(cY+2))
+					cell_create(str(cX)+','+str(cY-2))
+					cell_create(str(cX)+','+str(cY))
 					done = True
 				if cType == 'T':
-					cell_Create(str(cX-1)+','+str(cY))
-					cell_Create(str(cX+1)+','+str(cY)) 
-					cell_Create(str(cX)+','+str(cY)) 
-					cell_Create(str(cX)+','+str(cY+1))
-					cell_Create(str(cX)+','+str(cY+2))			
+					cell_create(str(cX-1)+','+str(cY))
+					cell_create(str(cX+1)+','+str(cY)) 
+					cell_create(str(cX)+','+str(cY)) 
+					cell_create(str(cX)+','+str(cY+1))
+					cell_create(str(cX)+','+str(cY+2))			
 					done = True
 				if cType == 'V':
-					cell_Create(str(cX+1)+','+str(cY+1))
-					cell_Create(str(cX+2)+','+str(cY+2))
-					cell_Create(str(cX-1)+','+str(cY-1))
-					cell_Create(str(cX-2)+','+str(cY-2))
-					cell_Create(str(cX)+','+str(cY))
+					cell_create(str(cX+1)+','+str(cY+1))
+					cell_create(str(cX+2)+','+str(cY+2))
+					cell_create(str(cX-1)+','+str(cY-1))
+					cell_create(str(cX-2)+','+str(cY-2))
+					cell_create(str(cX)+','+str(cY))
 					done = True
 				if cType == '+':
-					cell_Create(str(cX-1)+','+str(cY))					
-					cell_Create(str(cX+1)+','+str(cY))
-					cell_Create(str(cX)+','+str(cY+1))
-					cell_Create(str(cX)+','+str(cY-1))
-					cell_Create(str(cX)+','+str(cY))
+					cell_create(str(cX-1)+','+str(cY))					
+					cell_create(str(cX+1)+','+str(cY))
+					cell_create(str(cX)+','+str(cY+1))
+					cell_create(str(cX)+','+str(cY-1))
+					cell_create(str(cX)+','+str(cY))
 					done = True
-
-			
 			except:
 				print 'Something arent right... Trying again...'
 				pass
-
-###################### Check if some space have some kinda of life
-def check_life(wx,wy):
+####################### Check if some space have some kinda of life
+def isalive_check(wx,wy):
 	try:
 		if mapc[str(wx)+','+str(wy)] == 'C':
 			return True
 	except:
 		pass
 	return False
-
-###################### Look up to find free spaces
-def check_free(wx,wy):
-	if not check_life(wx+2,wy):	
+####################### Look up to find free spaces on a 5x5 neighborhood
+def freespace_check(wx,wy):
+	if not isalive_check(wx+2,wy):	
 		return str(wx+2)+','+str(wy)
-	if not check_life(wx-2,wy):	
+	if not isalive_check(wx-2,wy):	
 		return str(wx-2)+','+str(wy)
-	if not check_life(wx,wy+2):	
+	if not isalive_check(wx,wy+2):	
 		return str(wx)+','+str(wy+2)
-	if not check_life(wx,wy-2):	
+	if not isalive_check(wx,wy-2):	
 		return str(wx)+','+str(wy-2)
-
-	if not check_life(wx+2,wy+2):	
+	if not isalive_check(wx+2,wy+2):	
 		return str(wx+2)+','+str(wy-2)
-	if not check_life(wx-2,wy-2):	
+	if not isalive_check(wx-2,wy-2):	
 		return str(wx-2)+','+str(wy-2)
-	if not check_life(wx-2,wy+2):	
+	if not isalive_check(wx-2,wy+2):	
 		return str(wx-2)+','+str(wy+2)
-	if not check_life(wx+2,wy-2):	
+	if not isalive_check(wx+2,wy-2):	
 		return str(wx+2)+','+str(wy-2)
-
-	if not check_life(wx+1,wy+2):	
+	if not isalive_check(wx+1,wy+2):	
 		return str(wx+1)+','+str(wy-2)
-	if not check_life(wx-1,wy-2):	
+	if not isalive_check(wx-1,wy-2):	
 		return str(wx-1)+','+str(wy-2)
-	if not check_life(wx-1,wy+2):	
+	if not isalive_check(wx-1,wy+2):	
 		return str(wx-1)+','+str(wy+2)
-	if not check_life(wx+1,wy-2):	
+	if not isalive_check(wx+1,wy-2):	
 		return str(wx+1)+','+str(wy-2)
-
-	if not check_life(wx+2,wy+1):	
+	if not isalive_check(wx+2,wy+1):	
 		return str(wx+2)+','+str(wy+1)
-	if not check_life(wx-2,wy-1):	
+	if not isalive_check(wx-2,wy-1):	
 		return str(wx-2)+','+str(wy-1)
-	if not check_life(wx-2,wy+1):	
+	if not isalive_check(wx-2,wy+1):	
 		return str(wx-2)+','+str(wy+1)
-	if not check_life(wx+2,wy-1):	
+	if not isalive_check(wx+2,wy-1):	
 		return str(wx+2)+','+str(wy-1)
-
-#
-
-	if not check_life(wx+1,wy):	
+	if not isalive_check(wx+1,wy):	
 		return str(wx+1)+','+str(wy)
-	if not check_life(wx-1,wy):	
+	if not isalive_check(wx-1,wy):	
 		return str(wx-1)+','+str(wy)
-	if not check_life(wx,wy+1):	
+	if not isalive_check(wx,wy+1):	
 		return str(wx)+','+str(wy+1)
-	if not check_life(wx,wy-1):	
+	if not isalive_check(wx,wy-1):	
 		return str(wx)+','+str(wy-1)
-	if not check_life(wx+1,wy+1):	
+	if not isalive_check(wx+1,wy+1):	
 		return str(wx+1)+','+str(wy-1)
-	if not check_life(wx+1,wy-1):	
+	if not isalive_check(wx+1,wy-1):	
 		return str(wx-1)+','+str(wy+1)
-	if not check_life(wx-1,wy+1):	
+	if not isalive_check(wx-1,wy+1):	
 		return str(wx+1)+','+str(wy+1)
-	if not check_life(wx-1,wy-1):	
+	if not isalive_check(wx-1,wy-1):	
 		return str(wx-1)+','+str(wy-1)
 	return 'none'
-
-
-
-
-def check_alive(wx,wy):
-	if check_life(wx+2,wy):	
+###################### Look up to find alive cells on a 5x5 neighborhood
+def alivecell_check(wx,wy):
+	if isalive_check(wx+2,wy):	
 		return str(wx+2)+','+str(wy)
-	if check_life(wx-2,wy):	
+	if isalive_check(wx-2,wy):	
 		return str(wx-2)+','+str(wy)
-	if check_life(wx,wy+2):	
+	if isalive_check(wx,wy+2):	
 		return str(wx)+','+str(wy+2)
-	if check_life(wx,wy-2):	
+	if isalive_check(wx,wy-2):	
 		return str(wx)+','+str(wy-2)
-
-	if check_life(wx+2,wy+2):	
+	if isalive_check(wx+2,wy+2):	
 		return str(wx+2)+','+str(wy-2)
-	if check_life(wx-2,wy-2):	
+	if isalive_check(wx-2,wy-2):	
 		return str(wx-2)+','+str(wy-2)
-	if check_life(wx-2,wy+2):	
+	if isalive_check(wx-2,wy+2):	
 		return str(wx-2)+','+str(wy+2)
-	if check_life(wx+2,wy-2):	
+	if isalive_check(wx+2,wy-2):	
 		return str(wx+2)+','+str(wy-2)
-
-	if check_life(wx+1,wy+2):	
+	if isalive_check(wx+1,wy+2):	
 		return str(wx+1)+','+str(wy-2)
-	if check_life(wx-1,wy-2):	
+	if isalive_check(wx-1,wy-2):	
 		return str(wx-1)+','+str(wy-2)
-	if check_life(wx-1,wy+2):	
+	if isalive_check(wx-1,wy+2):	
 		return str(wx-1)+','+str(wy+2)
-	if check_life(wx+1,wy-2):	
+	if isalive_check(wx+1,wy-2):	
 		return str(wx+1)+','+str(wy-2)
-
-	if check_life(wx+2,wy+1):	
+	if isalive_check(wx+2,wy+1):	
 		return str(wx+2)+','+str(wy+1)
-	if check_life(wx-2,wy-1):	
+	if isalive_check(wx-2,wy-1):	
 		return str(wx-2)+','+str(wy-1)
-	if check_life(wx-2,wy+1):	
+	if isalive_check(wx-2,wy+1):	
 		return str(wx-2)+','+str(wy+1)
-	if check_life(wx+2,wy-1):	
+	if isalive_check(wx+2,wy-1):	
 		return str(wx+2)+','+str(wy-1)
-
-#
-
-	if check_life(wx+1,wy):	
+	if isalive_check(wx+1,wy):	
 		return str(wx+1)+','+str(wy)
-	if check_life(wx-1,wy):	
+	if isalive_check(wx-1,wy):	
 		return str(wx-1)+','+str(wy)
-	if check_life(wx,wy+1):	
+	if isalive_check(wx,wy+1):	
 		return str(wx)+','+str(wy+1)
-	if check_life(wx,wy-1):	
+	if isalive_check(wx,wy-1):	
 		return str(wx)+','+str(wy-1)
-	if check_life(wx+1,wy+1):	
+	if isalive_check(wx+1,wy+1):	
 		return str(wx+1)+','+str(wy-1)
-	if check_life(wx+1,wy-1):	
+	if isalive_check(wx+1,wy-1):	
 		return str(wx-1)+','+str(wy+1)
-	if check_life(wx-1,wy+1):	
+	if isalive_check(wx-1,wy+1):	
 		return str(wx+1)+','+str(wy+1)
-	if check_life(wx-1,wy-1):	
+	if isalive_check(wx-1,wy-1):	
 		return str(wx-1)+','+str(wy-1)
 	return 'none'
-########################### Remove 3x3 cells, where wx and wy is the 0-ground
-def remove_neighbor(wx,wy):
-
-
-
-
-
-
-
-
+############################ remove a 5x5 neighborhood, where wx and wy r the 0-groud
+def neighborhood_remove(wx,wy):
 	try:
-		 cell_Remove(wx+2,wy)
+		 cell_remove(wx+2,wy)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-2,wy)
+		 cell_remove(wx-2,wy)
 	except:
 		pass
 	try:
-		 cell_Remove(wx,wy-2)
+		 cell_remove(wx,wy-2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx,wy+2)
-	except:
-		pass
-
-
-	try:
-		 cell_Remove(wx+2,wy+2)
+		 cell_remove(wx,wy+2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-2,wy-2)
+		 cell_remove(wx+2,wy+2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx+2,wy-2)
+		 cell_remove(wx-2,wy-2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-2,wy+2)
-	except:
-		pass
-
-
-
-	try:
-		 cell_Remove(wx+2,wy+1)
+		 cell_remove(wx+2,wy-2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-2,wy+1)
-	except:
-		pass
-	
-	try:
-		 cell_Remove(wx+2,wy-1)
+		 cell_remove(wx-2,wy+2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-2,wy-1)
-	except:
-		pass
-#
-
-	try:
-		 cell_Remove(wx+1,wy+2)
+		 cell_remove(wx+2,wy+1)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-1,wy+2)
+		 cell_remove(wx-2,wy+1)
+	except:
+		pass
+	try:
+		 cell_remove(wx+2,wy-1)
+	except:
+		pass
+	try:
+		 cell_remove(wx-2,wy-1)
+	except:
+		pass
+	try:
+		 cell_remove(wx+1,wy+2)
+	except:
+		pass
+	try:
+		 cell_remove(wx-1,wy+2)
 	except:
 		pass
 	
 	try:
-		 cell_Remove(wx+1,wy-2)
+		 cell_remove(wx+1,wy-2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-1,wy-2)
-	except:
-		pass
-#####
-
-	try:
-		 cell_Remove(wx,wy)
+		 cell_remove(wx-1,wy-2)
 	except:
 		pass
 	try:
-		 cell_Remove(wx+1,wy)
+		 cell_remove(wx,wy)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-1,wy)
+		 cell_remove(wx+1,wy)
 	except:
 		pass
 	try:
-		 cell_Remove(wx,wy+1)
+		 cell_remove(wx-1,wy)
 	except:
 		pass
 	try:
-		 cell_Remove(wx,wy-1)
+		 cell_remove(wx,wy+1)
 	except:
 		pass
 	try:
-		 cell_Remove(wx-1,wy-1)
+		 cell_remove(wx,wy-1)
 	except:
 		pass
 	try:
-		 cell_Remove(wx+1,wy+1)
+		 cell_remove(wx-1,wy-1)
 	except:
 		pass
 	try:
- 		 cell_Remove(wx-1,wy+1)
+		 cell_remove(wx+1,wy+1)
 	except:
 		pass
 	try:
-		 cell_Remove(wx+1,w-1)
+ 		 cell_remove(wx-1,wy+1)
 	except:
 		pass
-##
-########################## Check the cell neighbor
-def check_neighbor(wx,wy):
-	
-		nLevel = 1
+	try:
+		 cell_remove(wx+1,w-1)
+	except:
+		pass
+########################### Check the cell neighborhood
+def neighborhood_check(wx,wy):
+		nLevel = 1 # at this moment i don't remind why i've seted this var to 1, but if aren't broken, don't fix 
 		try:
 			if mapc[str(wx+2)+','+str(wy)] == 'C':
 				nLevel = nLevel+1
@@ -364,7 +332,6 @@ def check_neighbor(wx,wy):
 				nLevel = nLevel+1
 		except:
 			pass
-
 		try:
 			if mapc[str(wx+1)+','+str(wy)] == 'C':
 				nLevel = nLevel+1
@@ -425,7 +392,6 @@ def check_neighbor(wx,wy):
 				nLevel = nLevel+1
 		except:
 			pass
-############################################################################
 		try:
 			if mapc[str(wx+2)+','+str(wy+2)] == 'C':
 				nLevel = nLevel+1
@@ -446,7 +412,6 @@ def check_neighbor(wx,wy):
 				nLevel = nLevel+1
 		except:
 			pass
-#############################
 		try:
 			if mapc[str(wx+2)+','+str(wy+1)] == 'C':
 				nLevel = nLevel+1
@@ -462,7 +427,6 @@ def check_neighbor(wx,wy):
 				nLevel = nLevel+1
 		except:
 			pass
-###############################
 		try:
 			if mapc[str(wx+1)+','+str(wy+2)] == 'C':
 				nLevel = nLevel+1
@@ -478,7 +442,6 @@ def check_neighbor(wx,wy):
 				nLevel = nLevel+1
 		except:
 			pass
-#
 		try:
 			if mapc[str(wx-2)+','+str(wy+1)] == 'C':
 				nLevel = nLevel+1
@@ -494,7 +457,6 @@ def check_neighbor(wx,wy):
 				nLevel = nLevel+1
 		except:
 			pass
-###############################
 		try:
 			if mapc[str(wx+1)+','+str(wy-2)] == 'C':
 				nLevel = nLevel+1
@@ -510,42 +472,35 @@ def check_neighbor(wx,wy):
 				nLevel = nLevel+1
 		except:
 			pass
-
 		return nLevel
 ################### remove a cell on a POG'ed 2D array
-def cell_Remove(wx,wy):
+def cell_remove(wx,wy):
 	mapc[str(wx)+','+str(wy)] = '-'
-
-################### Create a cell on a POG'ed 2D array
-def alias_remove(w):
+##################### yep, the name is TOO MUCH intuitive
+def fcell_remove(w):
 	mapc[w] = '-'
-def cell_Create(w):
-	
+#################### create a cell on a POG'ed 2D array
+def cell_create(w):
 	mapc[w] = 'C'
-
-############# Update map function, removing and creating cell		
+############## Update map function, removing and creating cell		
 def update():
 	global life
 	global turns
-
 	global x
 	global y
 	d = 1
 	for i in range(0,y+1):
 		for j in range(0,x+1):
 			d = d * (-1)
-			if check_life(j,i):
+			if isalive_check(j,i):
 					
-				if check_neighbor(j,i) <= 6 and  check_neighbor(j,i) > 1 :
-					cell_Create(check_free(j,i))
+				if neighborhood_check(j,i) <= 6 and  neighborhood_check(j,i) > 1 :
+					cell_create(freespace_check(j,i))
 					life = life+1
-				elif check_neighbor(j,i) > 6:
-					alias_remove(check_alive(j,i))
+				elif neighborhood_check(j,i) > 6:
+					fcell_remove(alivecell_check(j,i))
 				else:
-					remove_neighbor(j,i)
-					
-					
-
+					neighborhood_remove(j,i)	
 	if life == 0:
 		print 'The organism has been destroyed'
 		print 'Life time ' + str(turns)
@@ -553,19 +508,16 @@ def update():
 	else:
 		life = 0
 		turns = turns +1
-				
-	
-					
 
-init_life() #Call Game init
-
-
-#####Keep the map updating
+#######################################################################################################################
+################################################### BELLOW THE MAGIC HAPPENS... #######################################
+#######################################################################################################################
+life_init() #Call Game init
+############ Keep the map updating
 while True:
-	
-	print get_Map()
+	print map_get()
 	update()
-	time.sleep(0.01)
+	time.sleep(updTime)
 
 
 
